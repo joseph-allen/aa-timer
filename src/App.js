0f91079f0485c7903659343d4809c508acdac5dc
Joe RN <joseph.allen.1994@hotmail.com>
2020-05-12 15:29:34 +0100

    Add GA

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Timer from "./Components/Timer/Timer.jsx";
+import ReactGA from "react-ga";
 import "./App.css";
+ReactGA.initialize("UA-165720418-1");
 
 function App() {
   return (
